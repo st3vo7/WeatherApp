02c365577df5361fa68b785efa038873707a08f6
st3vo7 <stefan.maric@limecraft.com>
2021-12-24 12:17:52 +0100

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const forecast = require("../utils/forecast");
 const geocode = require("../utils/geocode");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // paths for Express cfg
 const publicDirPath = path.join(__dirname, "../public");
@@ -83,6 +84,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000.");
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
 });
